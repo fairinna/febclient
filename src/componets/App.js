4b4471c90dabb240a144f908133e081e563a9a07
Inna Namatov <inna.namatov@gmail.com>
2022-08-07 18:34:10 +0300

    add components Stream

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
-  const pageOne = () => {
-    return (
-      <div>
-        <h1>PAGEONE</h1>
-        <Link to='/page'>Go to the SECOND page</Link>
-      </div>
-    );
-  };
-  const pageTwo = () => {
-    return (
-      <div>
-        <h1>PAGE TWO</h1>
-        <button>CLIKC ME</button>
-        <Link to='/'>HOME PAGE</Link>
-      </div>
-    );
-  };
-
   return (
-    <div>
+    <div className='ui container'>
       <BrowserRouter>
-        <Route path='/' exact component={pageOne} />
-        <Route path='/page' component={pageTwo} />
+        <div>
+          <Header />
+          <Route path='/' exact component={StreamList}></Route>
+          <Route path='/streams/new' exect component={StreamCreate}></Route>
+          <Route path='/streams/edit' exect component={StreamEdit}></Route>
+          <Route path='/streams/delete' exect component={StreamDelete}></Route>
+          <Route path='/streams/show' exect component={StreamShow}></Route>
+        </div>
       </BrowserRouter>
     </div>
   );
