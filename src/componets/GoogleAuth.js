648069b890de3409563571f9f95889b1f6fe019c
Inna Namatov <inna.namatov@gmail.com>
2022-08-14 14:42:55 +0300

    using redux to the signIN/OUT

--- src/componets/GoogleAuth.js
+++ src/componets/GoogleAuth.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../action';
 
 class GoogleAuth extends React.Component {
-  state = { authGoogleProperty: null, authGoogleListen: false };
+  state = {
+    authGoogleProperty: null,
+    authGoogleListen: false,
+  };
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client
@@ -23,20 +28,28 @@ class GoogleAuth extends React.Component {
   listenSignedIn = () => {
     this.setState({ authGoogleProperty: this.authGoogle.isSignedIn.get() });
   };
+
+  onSignInClick = () => {
+    this.authGoogle.signIn();
+  };
+  onSignOutClick = () => {
+    this.authGoogle.signOut();
+  };
+
   showSignIn = () => {
     if (this.state.authGoogleProperty === null) {
       return null;
     }
     if (this.state.authGoogleProperty) {
       return (
-        <button className='ui red google button'>
-          <i className='google icon'> Sign OUT</i>
+        <button className='ui red google button' onClick={this.onSignOutClick}>
+          <i className='google icon'> </i> Sign OUT
         </button>
       );
     } else {
       return (
-        <button className='ui green google button'>
-          <i className='google icon'> SignIN</i>
+        <button className='ui green google button' onClick={this.onSignInClick}>
+          <i className='google icon'></i> SignIN
         </button>
       );
     }
@@ -45,4 +58,4 @@ class GoogleAuth extends React.Component {
     return <div className=''> {this.showSignIn()}</div>;
   }
 }
-export default GoogleAuth;
+export default connect(null, { signIn, signOut })(GoogleAuth);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './componets/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer';
 
+const store = createStore(reducer);
 const root = createRoot(document.querySelector('#root'));
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
